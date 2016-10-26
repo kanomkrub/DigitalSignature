07b34aaca865b79167a146978568c18cf5d4e767
004655 <tweerath.p@g-able.com>
2016-10-26 17:52:18 +0700

    initial digitalsignature pdfd

---
+++ DigitalSignature.Tests/PDFUtilityTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigitalSignatureService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalSignatureService.Models;
+
+namespace DigitalSignatureService.Tests
+{
+    [TestClass()]
+    public class PDFUtilityTests
+    {
+        [TestMethod()]
+        public void AddPDFFieldsTest()
+        {
+            var list = new List<PDFField>();
+            list.Add(new PdfTextField()
+            {
+                text = "HELLOOOOOOOOO",
+                x = 150,
+                y = 100,
+                width = 130,
+                height = 200,
+                fontSize = 25,
+                color = "#FF00008B",
+                showborder =true
+            });
+
+            PDFUtility.AddPDFFields(@"D:\temp\testSignatureService\BG.pdf", @"D:\temp\testSignatureService\BG_Out.pdf", list);
+        }
+    }
+}
---
+++ DigitalSignature/Models/DigitalSignatureTemplate.cs
@@ -0,0 +1,59 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalSignatureService.Models
+{
+    public class DigitalSignatureTemplate
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<PDFField> pdfFields { get; set; }
+    }
+    public abstract class PDFField
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public float x { get; set; } = 100;
+        public float y { get; set; } = 100;
+        public float width { get; set; } = 150;
+        public float height { get; set; } = 300;
+        public PDFFieldType type = PDFFieldType.TextField;
+        public PDFPage page = PDFPage.First;
+        public enum PDFPage
+        {
+            All,First,Last,Even,Odd,SpecificPage
+        }
+        public enum PDFFieldType
+        {
+            TextField,ImageField,SignatureField
+        }
+    }
+    public class PdfTextField : PDFField
+    {
+        public int fontSize { get; set; } = 14;
+        public string fontName { get; set; } = "HELVETICA_BOLD";
+        public int fontstyle { get; set; } = Font.NORMAL;
+        public string fontEncoding { get; set; } = "CP1252";
+        public bool fontEmbeded { get; set; } = true;
+        /// <summary>
+        /// argb format
+        /// </summary>
+        public string color { get; set; } = "#FF000000"; //black
+        public int align { get; set; }
+        public string text { get; set; }
+        public bool showborder { get; set; }
+    }
+    public class PdfImageField : PDFField
+    {
+
+    }
+    public class PdfSignatureField : PDFField
+    {
+
+
+    }
+}
